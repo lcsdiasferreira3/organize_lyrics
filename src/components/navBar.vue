4cbc5cb378e654eef581faa3cdcc949ee2334c2e
lcsdiasferreira <lcsdferreira3@gmail.com>
2022-09-29 15:49:26 -0300

    init project

---
+++ src/components/navBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar elevation="4">
+    <v-col class="d-flex justify-center align-center">
+      <v-col cols="3" class="mt-7">
+        <v-text-field single-line outlined rounded></v-text-field>
+      </v-col>
+    </v-col>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
