1e23eb2292d93aecc8d695f1721a86d14339a486
lcsdiasferreira <lcsdferreira3@gmail.com>
2022-10-03 16:14:26 -0300

    improve the style

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,9 +1,17 @@
 <template>
-  <v-app-bar elevation="4">
-    <v-col class="d-flex justify-center align-center">
-      <v-col cols="3" class="mt-7">
-        <v-text-field single-line outlined rounded></v-text-field>
-      </v-col>
+  <v-app-bar elevation="4" height="60%" dark>
+    <v-col cols="12">
+      <v-row class="d-flex justify-center align-center">
+        <v-col cols="2" class="d-flex justify-end align-center">
+          <img src="../assets/music.png" width="70px" />
+        </v-col>
+        <v-col cols="4" class="mt-7">
+          <v-text-field single-line solo-inverted></v-text-field>
+        </v-col>
+        <v-col cols="2" class="d-flex align-center pl-0 pr-0">
+          <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>
+        </v-col>
+      </v-row>
     </v-col>
   </v-app-bar>
 </template>
@@ -12,4 +20,10 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.v-text-field.v-text-field--solo:not(.v-text-field--solo-flat)
+  > .v-input__control
+  > .v-input__slot {
+  transition: 0.5s !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,59 @@
 <template>
-  <div class="home"></div>
+  <v-content class="back">
+    <v-col cols="12">
+      <v-row>
+        <v-col cols="6">
+          <v-row class="d-flex justify-center">
+            <v-col cols="7"
+              ><div>
+                <v-card dark>
+                  <v-card-title class="d-flex justify-center"
+                    >Customize the Lyric</v-card-title
+                  >
+                  <v-divider></v-divider>
+                  <v-card-title>Music</v-card-title>
+                  <v-divider></v-divider>
+                  <v-card-subtitle class="pb-0">teste</v-card-subtitle>
+                  <v-card-subtitle class="pt-0">teste</v-card-subtitle>
+                  <v-divider></v-divider>
+                </v-card></div
+            ></v-col>
+          </v-row>
+          <v-row class="d-flex justify-center">
+            <v-col cols="7"
+              ><div>
+                <v-card dark>
+                  <v-card-title class="d-flex justify-center"
+                    >List of the Songs to export</v-card-title
+                  >
+                  <v-divider></v-divider>
+                  <v-card-title>1# Music</v-card-title>
+                  <v-card-subtitle class="">teste</v-card-subtitle>
+                  <v-divider></v-divider>
+                  <v-divider></v-divider>
+                  <v-card-title>2# Music</v-card-title>
+                  <v-card-subtitle class="">teste</v-card-subtitle>
+                  <v-divider></v-divider>
+                </v-card></div
+            ></v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="6">
+          <div class="d-flex justify-center">
+            <v-col cols="9">
+              <v-card dark min-height="500px">
+                <v-card-title class="d-flex justify-center">Songs</v-card-title>
+                <v-divider></v-divider>
+                <div class="d-flex align-center justify-center siz">
+                  <v-card-subtitle>Search for any song!</v-card-subtitle>
+                </div>
+              </v-card>
+            </v-col>
+          </div></v-col
+        >
+      </v-row>
+    </v-col>
+  </v-content>
 </template>
 
 <script>
@@ -10,3 +64,14 @@ export default {
   components: {},
 };
 </script>
+
+<style scoped>
+.back {
+  background: linear-gradient(to bottom, #025159, #7ec8d9);
+  height: 100vh;
+}
+.siz {
+  height: 500px;
+  width: 100%;
+}
+</style>
